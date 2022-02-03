c66d82f2edbb7eaa7f44ac1b86b21583413040c8
Елена <etretjakowa@yandex.ru>
2022-02-03 23:37:21 +0700

    ДЗ 2.11

---
+++ src/main/java/course2/lesson2__11/Lesson211Application.java
@@ -0,0 +1,13 @@
+package course2.lesson2__11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson211Application.class, args);
+    }
+
+}
---
+++ src/test/java/course2/lesson2__11/Lesson211ApplicationTests.java
@@ -0,0 +1,13 @@
+package course2.lesson2__11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
